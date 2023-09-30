17e473dfde6094450f5efb9cdae9f4923a9befc5
JasonV <darkspeed9299@gmail.com>
2023-09-30 22:42:45 +0700

    Hit a wall

--- AI/BT Test.py
+++ AI/BT Test.py
@@ -1,39 +1,24 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the Iris dataset
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-# You can also download the dataset and save it locally if needed.
+# Step 1: Load the iris.csv file into a Pandas DataFrame
+iris_df = pd.read_csv('iris.csv')
 
-# Define column names
-column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'variety']
-
-# Read the CSV file into a pandas DataFrame
-iris_df = pd.read_csv(url, names=column_names)
-
-# Display the first few rows of the dataset to verify it was loaded correctly
-print(iris_df.head())
-
-# Visualize the data (scatter plot)
-plt.figure(figsize=(10, 6))
-
-# Create separate DataFrames for each variety
-setosa_df = iris_df[iris_df['variety'] == 'Iris-setosa']
-versicolor_df = iris_df[iris_df['variety'] == 'Iris-versicolor']
-virginica_df = iris_df[iris_df['variety'] == 'Iris-virginica']
-
-# Plot sepal length vs. sepal width
-plt.scatter(setosa_df['sepal_length'], setosa_df['sepal_width'], label='Setosa', marker='o')
-plt.scatter(versicolor_df['sepal_length'], versicolor_df['sepal_width'], label='Versicolor', marker='s')
-plt.scatter(virginica_df['sepal_length'], virginica_df['sepal_width'], label='Virginica', marker='^')
-
-# Add labels and a legend
+# a) Plot the relations of the dimensions of Setosa's sepal
+setosa_df = iris_df[iris_df['variety'] == 'Setosa']
+plt.scatter(setosa_df['sepal_length'], setosa_df['sepal_width'])
 plt.xlabel('Sepal Length (cm)')
 plt.ylabel('Sepal Width (cm)')
-plt.legend(loc='best')
-plt.title('Sepal Length vs. Sepal Width')
+plt.title('Setosa Sepal Dimensions')
+plt.show()
 
-# Show the plot
-#plt.show()
-plt.savefig('petal.png', dpi=300)
+# b) Plot the relations of the dimensions of Versicolor's petal
+versicolor_df = iris_df[iris_df['variety'] == 'Versicolor']
+plt.scatter(versicolor_df['petal_length'], versicolor_df['petal_width'])
+plt.xlabel('Petal Length (cm)')
+plt.ylabel('Petal Width (cm)')
+plt.title('Versicolor Petal Dimensions')
+plt.show()
+
+# c) Save the information of the lengths and widths of Versicolor into a versicolor.csv file
+versicolor_df[['petal_length', 'petal_width']].to_csv('versicolor.csv', index=False)
