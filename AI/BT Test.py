9808acc93cb6804382ba5404df7bd509a7f608c9
JasonV <darkspeed9299@gmail.com>
2023-10-01 00:32:29 +0700

    Bicc wall

--- AI/BT Test.py
+++ AI/BT Test.py
@@ -1,24 +1,17 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Step 1: Load the iris.csv file into a Pandas DataFrame
-iris_df = pd.read_csv('iris.csv')
 
-# a) Plot the relations of the dimensions of Setosa's sepal
-setosa_df = iris_df[iris_df['variety'] == 'Setosa']
+
+iris_df = pd.read_csv('D:\\CB\\AI\\iris.csv')
+
+setosa_df = iris_df[iris_df['variety'] == 'Iris-setosa']
+
+
 plt.scatter(setosa_df['sepal_length'], setosa_df['sepal_width'])
+plt.title('Sepal Dimensions of Iris-setosa')
 plt.xlabel('Sepal Length (cm)')
 plt.ylabel('Sepal Width (cm)')
-plt.title('Setosa Sepal Dimensions')
+plt.grid(True)
 plt.show()
-
-# b) Plot the relations of the dimensions of Versicolor's petal
-versicolor_df = iris_df[iris_df['variety'] == 'Versicolor']
-plt.scatter(versicolor_df['petal_length'], versicolor_df['petal_width'])
-plt.xlabel('Petal Length (cm)')
-plt.ylabel('Petal Width (cm)')
-plt.title('Versicolor Petal Dimensions')
-plt.show()
-
-# c) Save the information of the lengths and widths of Versicolor into a versicolor.csv file
-versicolor_df[['petal_length', 'petal_width']].to_csv('versicolor.csv', index=False)
