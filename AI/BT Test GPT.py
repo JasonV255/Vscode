5674638bfbeee83fabafa9e4b42472bc56702d9f
JasonV <darkspeed9299@gmail.com>
2023-10-01 08:22:56 +0700

    Class(1/10/2023)

--- AI/BT Test GPT.py
+++ AI/BT Test GPT.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-
-
-
-#iris_df = pd.read_csv('D:\\CB\\AI\\iris.csv')
+import matplotlib.pyplot as plt
+# Load the Iris dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# You can also download the dataset and save it locally if needed.
 
 # Define column names
 column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'variety']
 
 # Read the CSV file into a pandas DataFrame
-iris_df = pd.read_csv('D:\\CB\\AI\\iris.csv', names=column_names)
+iris_df = pd.read_csv(url, names=column_names)
 
 # Display the first few rows of the dataset to verify it was loaded correctly
 print(iris_df.head())
@@ -24,9 +23,9 @@ versicolor_df = iris_df[iris_df['variety'] == 'Iris-versicolor']
 virginica_df = iris_df[iris_df['variety'] == 'Iris-virginica']
 
 # Plot sepal length vs. sepal width
-plt.plot(setosa_df['sepal_length'], setosa_df['sepal_width'], label='Setosa', marker='.')
-plt.plot(versicolor_df['sepal_length'], versicolor_df['sepal_width'], label='Versicolor', marker='s')
-plt.plot(virginica_df['sepal_length'], virginica_df['sepal_width'], label='Virginica', marker='^')
+plt.scatter(setosa_df['sepal_length'], setosa_df['sepal_width'], label='Setosa', marker='o')
+plt.scatter(versicolor_df['sepal_length'], versicolor_df['sepal_width'], label='Versicolor', marker='s')
+plt.scatter(virginica_df['sepal_length'], virginica_df['sepal_width'], label='Virginica', marker='^')
 
 # Add labels and a legend
 plt.xlabel('Sepal Length (cm)')
